8437da2af5408af0f06571d85052a3d672ec2cb9
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2023-06-28 23:17:20 +0530

    Added a simple simple linked list, with just an append functon

--- basic/hello.zig
+++ basic/hello.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const print = std.debug.print;
+const expect = std.testing.expect;
 
 pub fn main() void {
+    const a = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
     print("Hello WOrld\n", .{});
+    print("{s}\n", .{a});
 }
---
+++ linked_list.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+
+const Node = struct {
+    data: i64,
+    next: ?*Node,
+};
+
+fn createNode(data: i64, allocator: std.mem.Allocator) !*Node {
+    var new_node = try allocator.create(Node);
+    new_node.data = data;
+    new_node.next = null;
+    return new_node;
+}
+
+const LinkedList = struct {
+    head: ?*Node = null,
+    allocator: std.mem.Allocator,
+
+    fn addNode(self: *LinkedList, data: i64) !void {
+        var new_node = try createNode(data, self.allocator);
+        if (self.head == null) {
+            self.head = new_node;
+            return;
+        }
+        var temp = self.head;
+        while (temp.?.next != null) {
+            temp = temp.?.next;
+        }
+        temp.?.next = new_node;
+    }
+
+    fn display(self: LinkedList) !void {
+        var temp = self.head;
+        while (temp) |v| {
+            try stdout.print("Node: {d}\n", .{v.data});
+            temp = v.next;
+        }
+    }
+
+    fn destroy(self: *LinkedList) !void {
+        var temp: ?*Node = undefined;
+        while (self.head) |v| {
+            temp = self.head;
+            self.head = v.next;
+            self.allocator.destroy(temp.?);
+        }
+    }
+};
+
+pub fn main() !void {
+    var allocator = std.heap.page_allocator;
+
+    var list = LinkedList{
+        .allocator = allocator,
+    };
+    try list.addNode(3);
+    try list.addNode(4);
+    try list.display();
+    try list.destroy();
+
+    try stdout.print("Hello World\n", .{});
+}
